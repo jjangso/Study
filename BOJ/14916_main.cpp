57f0d8ca04f24665caa2a8508c3cbbf3674c6ff6
jjangso <jjangso_ov@naver.com>
2021-12-27 22:30:16 +0900

    2021-12-27 14916 거스름돈 - Dynamic Programming

---
+++ BOJ/14916_main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int iCost = 0;
+	cin >> iCost;
+
+	int iResult = -1;
+	int iDiv = iCost / 5;
+	for (int i = iDiv; i >= 0; i--) {
+		int iSub = iCost - (i * 5);
+		if (iSub % 2 == 0) {
+			iResult = i + (iSub / 2);
+			break;
+		}
+	}
+
+	cout << iResult << "\n";
+
+	return 0;
+}
