943d914019722f5adfa3ed843be2c2fede658742
jjangso <jjangso_ov@naver.com>
2021-12-29 01:03:03 +0900

    2021-12-29 11727 2×n 타일링 2 - Dynamic Programming

---
+++ BOJ/11727_main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int iCount = 0;
+	cin >> iCount;
+	vector<long long> vecResult = { 1, 3 };
+
+	if (iCount > 2) {
+		for (int i = 2; i < iCount; i++) {
+			long long llNum = ((2 * vecResult[i - 2]) + vecResult[i - 1]) % 10007;
+			vecResult.push_back(llNum);
+		}
+	}
+
+	cout << vecResult[iCount - 1] << "\n";
+	return 0;
+}
