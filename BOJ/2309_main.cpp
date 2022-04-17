c37cdcd8748576e352fc358da4c22b343fe9bcd8
jjangso <jjangso_ov@naver.com>
2022-04-18 01:24:26 +0900

    2022-04-18 2309 일곱 난쟁이 - Brute Force

---
+++ BOJ/2309_main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	vector<int> vecDwarf(9);
+	for (int i = 0; i < 9; i++) {
+		cin >> vecDwarf[i];
+	}
+	sort(vecDwarf.begin(), vecDwarf.end());
+
+	vector<int> vecResult(7);
+	do {
+		int iSum = 0;
+		for (int i = 6; i >=0 ; i--) {
+			iSum += vecDwarf[i];
+			vecResult[i] = vecDwarf[i];
+		
+			if (iSum > 100) {
+				break;
+			}
+		}
+
+		if (iSum == 100) {
+			break;
+		}
+
+	} while (next_permutation(vecDwarf.begin(), vecDwarf.end()));
+
+	for (const auto& height : vecResult) {
+		cout << height << endl;
+	}
+
+	return 0;
+}
