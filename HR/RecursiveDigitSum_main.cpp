a002cf163cf69007eeadba61681888f5282b4454
jjangso <jjangso_ov@naver.com>
2022-05-29 01:59:09 +0900

    2022-05-29 Recursive Digit Sum - Recursion

---
+++ HR/RecursiveDigitSum_main.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+* Complete the 'superDigit' function below.
+*
+* The function is expected to return an INTEGER.
+* The function accepts following parameters:
+*  1. STRING n
+*  2. INTEGER k
+*/
+
+int superDigit(string n, int k) {
+	if (n.size() <= 1) {
+		return stoi(n);
+	}
+
+	long long llResult = 0;
+	for (int i = 0; i < n.size(); i++) {
+		llResult += (n[i] - '0');
+	}
+
+	if (k >= 1) {
+		llResult *= k;
+	}
+	return superDigit(to_string(llResult), 0);
+}
