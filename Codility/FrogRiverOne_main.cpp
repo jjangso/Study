6bfab49f02c77d5d868dfce4c1695cff5c87606a
jjangso <jjangso_ov@naver.com>
2022-06-12 05:27:44 +0900

    2022-06-12 FrogRiverOne

---
+++ Codility/FrogRiverOne_main.cpp
@@ -0,0 +1,20 @@
+﻿int solution(int X, vector<int> &A) {
+    vector<bool> vecLeaves(X + 1, false);
+    int iCount = X;
+    int iResult = 0;
+    for ( ; iResult < A.size(); iResult++ ) {
+        const auto& leaf = A[iResult];
+        if ( X < leaf || vecLeaves[leaf] ) {
+            continue;
+        }
+
+        vecLeaves[leaf] = true;
+        iCount--;
+
+        if ( iCount == 0 ) {
+            break;
+        }
+    }
+
+    return iCount == 0 ? iResult : -1;
+}
