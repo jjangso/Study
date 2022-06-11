983f574b5786d364681891c5066130d68815cc0a
jjangso <jjangso_ov@naver.com>
2022-06-12 04:37:13 +0900

    2022-06-12 PermMissingElem

---
+++ Codility/PermMissingElem_main.cpp
@@ -0,0 +1,14 @@
+﻿#include <algorithm>
+
+int solution(vector<int> &A) {
+    sort(A.begin(), A.end());
+
+    int iResult = 1;
+    for ( ; iResult <= A.size(); iResult++ ) {
+        if ( A[iResult-1] != iResult ) {
+            break;
+        } 
+    }
+
+    return iResult;    
+}
