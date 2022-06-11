45b90a3533430a51a11f6addfd3068f81652d1fe
jjangso <jjangso_ov@naver.com>
2022-06-12 05:42:54 +0900

    2022-06-12 PermCheck

---
+++ Codility/PermCheck_main.cpp
@@ -0,0 +1,15 @@
+﻿#include <algorithm>
+
+int solution(vector<int> &A) {
+    sort(A.begin(), A.end());
+
+    bool bResult = true;
+    for ( int i = 0; i < A.size(); i++ ) {
+        if ( A[i] != (i + 1) ) {
+            bResult = false;
+            break;
+        }
+    }
+	
+    return bResult;
+}
