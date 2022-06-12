4234617ff2f6dd5e053d216665f36ad87eb97333
jjangso <jjangso_ov@naver.com>
2022-06-12 18:36:58 +0900

    2022-06-12 MissingInteger

---
+++ Codility/MissingInteger_main.cpp
@@ -0,0 +1,18 @@
+﻿#include <algorithm>
+
+int solution(vector<int> &A) {
+    sort(A.begin(), A.end());
+
+    int iResult = 1;
+    for ( const auto& num : A ) {
+        if ( num > 0 ) {
+            if ( iResult < num ) {
+                break;
+            } else if ( iResult == num ) {
+                iResult++;
+            }
+        }
+    }
+
+    return iResult;
+}
