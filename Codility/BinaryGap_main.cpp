d8120c06d7621824696fd54f6049d47250f6cb7d
jjangso <jjangso_ov@naver.com>
2022-06-14 00:26:49 +0900

    2022-06-14 BinaryGap

---
+++ Codility/BinaryGap_main.cpp
@@ -0,0 +1,27 @@
+﻿// https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+
+int solution(int N) {
+    int iResult = 0;
+    int iCount = -1;
+	
+    while (true) {
+        if ( N & 1 ) {
+            if( iCount > iResult ) {
+                iResult = iCount;
+            } 
+            iCount = 0;
+        } else {
+            if ( iCount != -1 ) {
+                iCount++;
+            }
+        }
+        
+        N = N >> 1;
+        
+        if ( N <= 0 ) {
+            break;
+        }
+    }
+	
+    return iResult;
+}
