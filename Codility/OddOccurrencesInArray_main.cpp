91608fdbea6a8bf6169230c28113aed536dda006
jjangso <jjangso_ov@naver.com>
2022-06-14 00:38:01 +0900

    2022-06-14 OddOccurrencesInArray

---
+++ Codility/OddOccurrencesInArray_main.cpp
@@ -0,0 +1,27 @@
+﻿// https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+// Detected time complexity: O(N) or O(N*log(N))
+
+#include <algorithm>
+
+int solution(vector<int> &A) {
+    sort(A.begin(), A.end());
+
+    int iNum = 0;
+    int iCount = 0;
+    
+    for ( int i = 0; i < A.size(); i++ ) {
+        if ( iNum == A[i] ) {
+            iCount++;
+            continue;
+        }
+
+        if ( iCount != 0 && (iCount % 2) == 1 ) {
+            break;
+        }
+        
+        iNum = A[i];
+        iCount = 1;
+    }
+
+    return iNum;
+}
