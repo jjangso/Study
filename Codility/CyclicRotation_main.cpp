2de5e36b5ab6cc52b87a3c48a2c3da9920c4a5ff
jjangso <jjangso_ov@naver.com>
2022-06-14 00:32:02 +0900

    2022-06-14 CyclicRotation

---
+++ Codility/CyclicRotation_main.cpp
@@ -0,0 +1,19 @@
+﻿// https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+
+vector<int> solution(vector<int> &A, int K) {
+    vector<int> vecResult = A;
+
+    if ( A.size() != 0 && K != 0 ) {
+        int iRotate = K < A.size() ? K : K % A.size();  
+          
+        for ( int i = 0; i < A.size(); i++ ) {
+            int iPos = i + iRotate;
+            if ( iPos >= A.size() ) {
+                iPos -= A.size();
+            }
+            vecResult[iPos] = A[i];
+        }
+    }
+
+    return vecResult;
+}
