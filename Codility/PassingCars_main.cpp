6c114f59de48a8b5946efadf0ca28de034a97f2e
jjangso <jjangso_ov@naver.com>
2022-06-14 19:50:35 +0900

    2022-06-14 PassingCars

---
+++ Codility/PassingCars_main.cpp
@@ -0,0 +1,18 @@
+﻿// https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/
+// Detected time complexity: O(N)
+
+int solution(vector<int> &A) {
+    int iPCount = 0;
+    int iResult = 0;
+	
+    for ( const auto& car : A ) {
+        car == 0 ? iPCount++ : iResult += iPCount;
+
+        if ( iResult > 1000000000 ) {
+            iResult = -1;
+            break;
+        }
+    }
+	
+    return iResult;
+}
