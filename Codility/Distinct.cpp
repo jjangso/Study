7d46c9c8c658b04407c7682a750d67e802d41a81
jjangso <jjangso_ov@naver.com>
2022-06-21 00:22:18 +0900

    2022-06-21 Distinct

---
+++ Codility/Distinct.cpp
@@ -0,0 +1,18 @@
+﻿// https://app.codility.com/programmers/lessons/6-sorting/distinct/
+// Detected time complexity: O(N*log(N)) or O(N)
+
+#define MAX 1000000
+
+int solution(vector<int> &A) {
+    vector<bool> vecExist(2000001, false);
+
+    int iResult = 0;
+    for ( const auto& num : A ) {
+        if ( !vecExist[num + MAX] ) {
+            vecExist[num + MAX] = true;
+            iResult++;
+        }
+    }
+
+    return iResult;
+}
